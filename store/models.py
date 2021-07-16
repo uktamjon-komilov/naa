6253d43e1c14cad5e397e466df1575f7fdd9980b
Uktamjon <uktamforjob@gmail.com>
2021-07-16 23:20:33 +0500

    book model added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    rating = models.FloatField()
+    stock = models.IntegerField()
+    price = models.FloatField(default=1000.0)
+    date = models.DateField()
+
+
+
+# User -> id, first_name, last_name, email, phone, password, is_active, is_admin, is_staff, is_superuser
+
+# Product -> id, name, price, stock, description, rating, coupon, created_at, updated_at
+
+# Category -> id, name, created_at, updated_at
