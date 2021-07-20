7120fc18a8aeed0033096a4e3ef6b789b9e657ae
Uktamjon <uktamforjob@gmail.com>
2021-07-20 20:52:36 +0500

    5-lesson start

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -102,11 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "jamlangan")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-    os.path.join(BASE_DIR, "onlineshop", "uktamjon")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
 
 
